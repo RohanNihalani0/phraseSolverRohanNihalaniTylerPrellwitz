529f7400cb339b6fdd6b42733cdcaa3d609ea9b8
Tyler Prellwitz <tylerprellwitz@gmail.com>
2023-10-30 18:30:18 -0700

    test

--- Player.java
+++ Player.java
@@ -1,12 +1,30 @@
+/*
+ * Activity 2.5.2
+ * 
+ * A Player class the PhraseSolverGame
+ */
+import java.util.Scanner;
 
-public class Player {
+public class Player
+{
+  /* your code here - attributes */
 
+  /* your code here - constructor(s) */ 
   public Player() {
-
+    System.out.println("Enter Player Name:");
+    Scanner sc = new Scanner(System.in);
+    String newName = sc.nextLine();
+    String name = newName;
+    System.out.println("Hello and welcome to the game, " + name);
+    int points = 0;
   }
 
-
-  public static void main(String[] args) {
-
+  public Player(String inputName) {
+    String name = inputName;
+    int points = 0;
+    System.out.println("Welcome " + name);
   }
+  /* your code here - accessor(s) */ 
+
+  /* your code here - mutator(s) */ 
 }
--- Runner.java
+++ Runner.java
@@ -1,10 +1,14 @@
-
-public class Runner {
-  public Runner() {
-
+/*
+ * Activity 2.5.2
+ *
+ * The runner for the PhraseSolverGame
+ */
+public class Runner
+{
+  public static void main(String[] args) 
+  {
+    PhraseSolver p = new PhraseSolver(); 
+    p.play();
   }
-  
-  public static void main(String[] args) {
-    
-  }
-}
+
+} 
