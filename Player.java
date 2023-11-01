fa9b63149b0b45c83a22c263ec2e5ccb4b110dbc
RohanNihalani0 <142432538+RohanNihalani0@users.noreply.github.com>
2023-11-01 13:20:54 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -10,19 +10,19 @@ public class Player
   /* your code here - attributes */
 
   /* your code here - constructor(s) */ 
-  public Player() {
-    System.out.println("Enter Player Name:");
+  public Player() { // constructor with no params
+    System.out.println("Enter Player Name:"); // prompts user to enter their name
     Scanner sc = new Scanner(System.in);
     String newName = sc.nextLine();
-    String name = newName;
-    System.out.println("Hello and welcome to the game, " + name);
-    int points = 0;
+    String name = newName; // sets name to what they enter
+    System.out.println("Hello and welcome to the game, " + name); // prints welcome
+    int points = 0; // sets points to start at 0
   }
 
   public Player(String inputName) {
-    String name = inputName;
-    int points = 0;
-    System.out.println("Welcome " + name);
+    String name = inputName; // sets name to whatever parameter is passed in
+    int points = 0; // starts points at 0
+    System.out.println("Welcome " + name); // prints welcome statement
   }
   /* your code here - accessor(s) */ 
 
